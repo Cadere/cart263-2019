888f7f063557ba52f723e0af31988a4b6d5dfa38
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-02-19 11:11:59 -0500

    A4 - A5: Created A4 from work in class, worked on A5 from template

--- assignments/template-project/js/script.js
+++ assignments/template-project/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /*****************
 
 Title of Project
@@ -7,31 +8,192 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-let $mouth;
-let $fly;
+let animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra"
+];
+let correctAnimal;
+let answers = [];
+const NUM_OPTIONS = 5;
 
-$(document).ready(setup);
+$(document).ready(function(){
+  $('#starter').on('click', function(){
+    $('#starter').remove();
+    startGame();
+  })
+});
 
-function setup(){
-  $mouth = $('#mouth');
-  $fly = $('#fly');
-
-  $fly.draggable();
-  $mouth.droppable({
-    drop: flyDropped
-  });
+function startGame(){
+  newRound();
 }
 
-function flyDropped(event,ui) {
-  ui.draggable.remove();
-  setInterval(chew, 200)
+function addButton(label){
+  let $button = $('<div class = "guess"> </div>')
+  $button.text(label);
+  $button.button();
+  $button.on('click', buttonClicked)
+  $('body').append($button);
 }
 
-function chew(){
-  if($mouth.attr('src') === 'assets/images/mouth-open.png'){
-    $mouth.attr('src','assets/images/mouth-closed.png')
+function buttonClicked(){
+  if ($(this).text() === correctAnimal) {
+    $('.guess').remove();
+    setTimeout(newRound,1000);
   }
-  else{
-    $mouth.attr('src','assets/images/mouth-open.png')
+  else {
+    speakAnimal(correctAnimal);
+    $('this').effect('shake');
   }
+}
+
+function newRound(){
+  answers = [];
+  for(let i = 0; i < NUM_OPTIONS; i++){
+    let guess = animals[Math.floor(Math.random() * animals.length)]
+    answers.push(guess);
+    addButton(guess);
+  }
+  correctAnimal = answers[Math.floor(Math.random()*answers.length)];
+  speakAnimal(correctAnimal);
+}
+
+function speakAnimal(name) {
+  let reverseName = name.split('').reverse().join('');
+  let options = {
+    rate: Math.random(),
+    pitch: Math.random()
+  }
+  responsiveVoice.speak(reverseName, "UK English Male", options);
 }
