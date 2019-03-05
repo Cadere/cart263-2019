7ce67375369ec3fd924a8efee47716e2aa3edb33
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-05 09:57:35 -0500

    P2: replaced assets with files of a fixed size

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -2,10 +2,13 @@
 
 /*****************
 
-Title of Project
+Title of Project: purpuseful gracelessness
 Author Name
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+function setup(){
+  
+}
