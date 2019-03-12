71d184237bee352277a73306be2aa9f9535edae0
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-12 10:12:28 -0400

    A6: Did in class part of assigment

--- assignments/assignment6/js/script.js
+++ assignments/assignment6/js/script.js
@@ -9,3 +9,30 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let verb = "is";
+
+$(document).ready(function () {
+  $.getJSON("data/data.json",dataLoaded)
+})
+
+function dataLoaded(data){
+  console.log(data);
+
+  let condiment = getRandomElement(data.condiments);
+  console.log(condiment)
+  if (condiment.charAt(condiment.length-1) === "s"){
+    verb = "are";
+  }
+  else {
+    verb = "is";
+  }
+  let cat = getRandomElement(data.cats);
+  let room = getRandomElement(data.rooms);
+  let description = `${condiment} ${verb} like a ${cat} in a ${room}`;
+  $('body').append('<p>'+description+'</p>');
+}
+
+function getRandomElement(array){
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
+}
