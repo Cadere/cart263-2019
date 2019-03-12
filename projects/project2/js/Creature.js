6a45aebd62e96791323f5cfe4151723dbf3b09ce
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-12 10:36:59 -0400

    P2: Changed the song to a template string

--- projects/project2/js/Creature.js
+++ projects/project2/js/Creature.js
@@ -25,9 +25,9 @@ Creature.prototype.modify = function(head, arm, leg, body){
 
 //buildSong() constructs the story from the keywords attached to each limb
 Creature.prototype.buildSong = function(){
-  this.song = "The " + this.arm.firstword + " " + this.head.firstword + " was " + this.arm.colorword + " on the " + this.head.colorword + "\n" +
-    "Soon " + this.head.secondword + " " + this.leg.colorword + " a " + this.leg.firstword + " " + this.arm.secondword + "\n" +
-    "The " + this.head.firstword + " " + this.body.colorword + " " + this.body.secondword + "\n" +
-    "The " + this.arm.secondword + " " + this.leg.secondword + "\n" +
-    "And " + this.head.secondword + " was left feeling all " + this.body.firstword
+  this.song = `The ${this.arm.firstword} ${this.head.firstword} was ${this.arm.colorword} on the ${this.head.colorword}
+    Soon ${this.head.secondword} ${this.leg.colorword} a ${this.leg.firstword} ${this.arm.secondword}
+    The ${this.head.firstword} ${this.body.colorword} ${this.body.secondword}
+    The ${this.arm.secondword} ${this.leg.secondword}
+    And ${this.head.secondword} was left feeling all ${this.body.firstword}`
 }
