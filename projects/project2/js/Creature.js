3c579e906b4899e721f3c6e70bd69f74882449dd
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-12 09:31:40 -0400

    A6: Added template file

--- projects/project2/js/Creature.js
+++ projects/project2/js/Creature.js
@@ -9,10 +9,10 @@ function Creature(head, arm, leg, body){
 
 //displays the parts of the creature
 Creature.prototype.display = function(){
-  image(this.body.image, width/2, height/2, creatureSize, creatureSize);
-  image(this.leg.image, width/2, height/2, creatureSize, creatureSize);
-  image(this.arm.image, width/2, height/2, creatureSize, creatureSize);
-  image(this.head.image, width/2, height/2, creatureSize, creatureSize);
+  image(this.body.image, 2*width/3, height/2, creatureSize, creatureSize);
+  image(this.leg.image, 2*width/3, height/2, creatureSize, creatureSize);
+  image(this.arm.image, 2*width/3, height/2, creatureSize, creatureSize);
+  image(this.head.image, 2*width/3, height/2, creatureSize, creatureSize);
 }
 
 //modify() changes the reference of the limbs
