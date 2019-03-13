ab37ad4b036400e58a8c6b36797368b445260ee7
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-12 22:45:49 -0400

    P2: created the colors object

---
+++ projects/project2/js/Colors.js
@@ -0,0 +1,8 @@
+//Colors constructor
+//
+//the colors object holds data which will be stashed into an array
+function Color(sky, hill, road){
+  this.sky = sky;
+  this.hill = hill;
+  this.road = road;
+}
