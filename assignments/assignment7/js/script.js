1c4a32bdc623aee4ed9382df2dfd824af2d2374c
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-19 09:14:26 -0400

    A7: Downloaded sound files

--- assignments/assignment7/js/script.js
+++ assignments/assignment7/js/script.js
@@ -9,3 +9,5 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let frequencies = [220.00,246.94,277.18,293.66,329.63,369.99,392.00];
+let frequencies2 = [440.00,493.88,554.37,587.33,659.25,739.99,830.61];
