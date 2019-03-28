7841d43751d9a7121d3300f1e86c7c9c0e420b95
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-28 19:53:24 -0400

    P3: started building a program that will help me build a json file

---
+++ projects/p3-data-extractor/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*****************
+
+P3 data extractor
+Eugène N. Fournier
+
+In project 3 I will be using a large JSON file with a lot of color values
+here I am building a small program that will process images and store the colors of pixels at specific locations
+
+******************/
