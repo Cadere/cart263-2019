2675c28af8e1dcc0a694b3d7be0c3af7b5d8674f
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2019-03-28 22:05:35 -0400

    P3: added some arrays for the data extracting code

--- projects/p3-data-extractor/js/script.js
+++ projects/p3-data-extractor/js/script.js
@@ -9,3 +9,16 @@ In project 3 I will be using a large JSON file with a lot of color values
 here I am building a small program that will process images and store the colors of pixels at specific locations
 
 ******************/
+// this variable is for the x positions of the pixels whose colors we want
+//the colors are in the following order: body, leg, belly, head, head2, violet, pattern, hoof, eye
+pixelX = [513,610,394,282,154,537,187,198,136];
+//this is the variable for the y positions of the pixels whose colors we want
+pixelY = [313,507,396,69,147,220,283,576,97];
+
+//this variable is for the phenotype names of each little creature in the image from which we extract colors
+phenotype = [
+  "yc","Yy","YC","Yv","yy","yN",
+  "yCD","YYD","YCD","YVD","yYD","YxD",
+  "yCd","YYd","YCd","YVd","yYd","Yxd",
+  "yCa","YYa","YCa","YVa","yYa","Yxd"
+]
